abf1db4322315cf4dc0be2cf39c605f407a40412
Eliord32 <elielguilherme2001@gmail.com>
2025-05-25 12:58:56 +0000

    Criei o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-    
-
 int main(){
-    printf("Desafio cartas!\n");
+
+    printf(" ***Super Trunfo*** \n");
     char estado[20];
     char codigo[5];
     char cidade[20];
     int populacao;
     float area;
     float PIB;
-    int pontos;
-
+    int  pontos;
+    float divisao;
+    float divisao2;
+    
+    // Cadastro da carta 1
     printf("Digite seu estado: \n");
     scanf("%s", &estado);
 
@@ -29,22 +31,30 @@ int main(){
     printf("Digite seu PIB: \n");
     scanf("%f", &PIB);
 
-    printf("Digite seus pontos: \n");
+    printf("Número de Pontos Turísticos: \n");
     scanf("%d", &pontos);
     
+    // Divisão da polulação, Àrea e PIB
+    divisao =  populacao / area;
+    divisao2 =  PIB / populacao;
+   
+    //  Carta 01 cadastrada
     printf("Carta 1! \n");
     printf("Estado: %s \n", estado);
-    printf("Código da Carta: %s \n", codigo);
+    printf("Código: %s \n", codigo);
     printf("Cidade: %s \n", cidade);
     printf("População: %d \n", populacao);
     printf("Área: %f \n", area);
     printf("PIB: %f \n", PIB);
-    printf("Pontos %d \n", pontos);
+    printf("Número de Pontos Turísticos %d \n", pontos);
+    printf("Densidade Populacional: %.3f \n", divisao);
+    printf("PIB per Capita: %d \n", divisao2);
 
+    // Cadastro da carta 2
     printf("Digite seu estado: \n");
     scanf("%s", &estado);
 
-    printf("Digite seu codigo: \n");
+    printf("Digite seu código: \n");
     scanf("%s", &codigo);
 
     printf("Digite sua cidade: \n");
@@ -59,9 +69,14 @@ int main(){
     printf("Digite seu PIB: \n");
     scanf("%f", &PIB);
 
-    printf("Digite seus pontos: \n");
+    printf("Número de Pontos Turísticos: \n");
     scanf("%d", &pontos);
-    
+
+    // Divisão da polulação, Àrea e PIB
+    divisao = populacao / area;
+    divisao2 = PIB / populacao;
+
+    // Carta 02 cadastrada
     printf("Carta: 2! \n");
     printf("Estado: %s \n", estado);
     printf("Código da Carta: %s \n", codigo);
@@ -69,9 +84,9 @@ int main(){
     printf("População: %d \n", populacao);
     printf("Área: %f \n", area);
     printf("PIB: %f \n", PIB);
-    printf("Pontos %d \n", pontos);
+    printf("Número de Pontos Turísticos %d \n", pontos);
+    printf("Densidade Populacional: %.3f \n", divisao);
+    printf("PIB per Capita: %d \n", divisao2);
 
-
-    return 0;
-     
+    return 0; 
 }
